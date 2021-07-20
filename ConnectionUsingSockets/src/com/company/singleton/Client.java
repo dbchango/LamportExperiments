f708f9037dd1afb60302c802e20483f998fe268b
David Chango <dbchango@espe.edu.ec>
2021-07-20 09:24:56 -0500

    Client and server classes - created

--- ConnectionUsingSockets/src/com/company/singleton/Client.java
+++ ConnectionUsingSockets/src/com/company/singleton/Client.java
@@ -1,9 +1,21 @@
 package com.company.singleton;
 
+import java.io.*;
 import java.net.*;
 
 public class Client {
-    private static Socket
 
+    public Client(){
+        try{
+            Socket socket = new Socket("10.0.0.5", 6666);
+            DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+            dout.writeUTF("Hello server");
+            dout.close();
+            socket.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+
+    }
 
 }
--- ConnectionUsingSockets/src/com/company/singleton/Server.java
+++ ConnectionUsingSockets/src/com/company/singleton/Server.java
@@ -1,4 +1,22 @@
 package com.company.singleton;
 
+import java.io.*;
+import java.net.*;
+
 public class Server {
+    private static int PORT = 9090;
+        public static void main(String[] args){
+            try{
+                ServerSocket listener = new ServerSocket(6666);
+                Socket s = listener.accept(); // acepta la peticion del cliente
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                String str = (String) dis.readUTF();
+                System.out.println("Clients says: " + str);
+                listener.close();
+
+            }catch(IOException ex){
+                ex.printStackTrace();
+            }
+
+        }
 }
